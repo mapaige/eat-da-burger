69c0eff544fe3dfe84f702d847392ed6049448f3
Marcia Paige <mapaige7@gmail.com>
2020-08-22 17:44:49 -0400

    Added files and databases

---
+++ config/orm.js
@@ -0,0 +1,6 @@
+// Import MySQL connection.
+var connection = require("../config/connection.js");
+
+
+
+      
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ("turkey burger ");
+
+INSERT INTO burgers (burger_name) VALUES ("turkey sloppy joe");
+
+INSERT INTO burgers (burger_name) VALUES
+("black bean burger");
